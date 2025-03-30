ecb513bc9f166fe8de674623e670a79492231349
ValeryArauco <141700272+ValeryArauco@users.noreply.github.com>
2025-03-29 21:22:41 -0400

    Remove unused imports

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -2,10 +2,8 @@ import { Component } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
-import { MatMenuModule } from '@angular/material/menu';
 import { RouterLink } from '@angular/router';
 import { CommonModule } from '@angular/common';
-import {MatSidenavModule} from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-navbar',
@@ -15,9 +13,7 @@ import {MatSidenavModule} from '@angular/material/sidenav';
     MatToolbarModule,
     MatButtonModule,
     MatIconModule,
-    MatMenuModule,
     RouterLink,
-    MatSidenavModule
   ],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
