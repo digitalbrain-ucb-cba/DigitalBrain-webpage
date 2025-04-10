b29944f18a3d0401e7000f11049f4e07da830051
RedoRojo <cristhiabnvictorrm@gmaill.com>
2025-04-09 20:14:36 -0400

    Presentation made

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { TeamsComponent } from './pages/teams/teams.component';
 
 export const routes: Routes = [
     {
         path: '',
         component: HomeComponent,
+    },
+    {
+        path: "teams",
+        component: TeamsComponent,
     }
 ];
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { BannerComponent } from './sections/banner/banner.component';
 import { TechStackComponent } from './sections/tech-stack/tech-stack.component';
+import { PresentationComponent } from "./sections/presentation/presentation.component";
 @Component({
   selector: 'app-home',
   imports: [
     BannerComponent,
-    TechStackComponent
-  ],
+    TechStackComponent,
+    PresentationComponent
+],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
