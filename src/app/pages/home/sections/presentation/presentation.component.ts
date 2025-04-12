d64b47cf0c4734f49469cacfdd3d639fd84089c3
ValeryArauco <141700272+ValeryArauco@users.noreply.github.com>
2025-04-12 00:35:34 -0400

    Update navigation presentation

--- src/app/pages/home/sections/presentation/presentation.component.ts
+++ src/app/pages/home/sections/presentation/presentation.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-presentation',
-  imports: [],
+  imports: [RouterLink, MatButtonModule],
   templateUrl: './presentation.component.html',
   styleUrl: './presentation.component.css'
 })
