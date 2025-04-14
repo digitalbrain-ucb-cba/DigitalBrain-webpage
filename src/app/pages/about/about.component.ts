e397e89f1e1da3a37801ca318a9b672f872f1fd1
RedoRojo <cristhiabnvictorrm@gmaill.com>
2025-04-14 16:44:54 -0400

    Our story section added

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { OurStoryComponent } from "./sections/our-story/our-story.component";
 
 @Component({
   selector: 'app-about',
-  imports: [],
+  imports: [OurStoryComponent],
   templateUrl: './about.component.html',
   styleUrl: './about.component.css'
 })
